fe7dc850ed643dac43484657ef42544de6f10a76
Markku Sukanen <markku.sukanen@gmail.com>
2025-08-03 18:50:50 +0300

    fixed serializers

--- src/world/area.rs
+++ src/world/area.rs
@@ -1,48 +1,48 @@
-use std::sync::Arc;
+//! Area stuff.
+use std::{collections::HashMap, sync::Arc};
 
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
-use crate::{DATA_PATH, traits::tickable::Tickable, world::room::{Room, area_room_serialization}};
+use crate::{traits::tickable::Tickable, world::room::{area_room_serialization, Room}, DATA_PATH};
 
-pub(crate) static AREA_PATH: Lazy<Arc<String>> = Lazy::new(|| Arc::new(format!("{}/areas", *DATA_PATH)));
+static AREA_PATH: Lazy<Arc<String>> = Lazy::new(|| Arc::new(format!("{}/areas", *DATA_PATH)));
 
 pub(crate) mod world_area_serialization {
-    use std::fs;
+    //! Serializer for [World] level [Area] listing.
+    use std::{collections::HashMap, fs};
 
     use serde::{Deserialize, Deserializer, Serialize, Serializer};
-    use crate::world::area::AREA_PATH;
 
-    use super::Area;
+    use super::{Area, AREA_PATH};
 
-    pub fn serialize<S: Serializer>(areas: &Option<Vec<Area>>, serializer: S) -> Result<S::Ok, S::Error>
+    pub fn serialize<S: Serializer>(areas: &HashMap<String, Area>, serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer,
     {
-        if let Some(areas) = areas {
-            let area_stems: Vec<String> = areas.iter().map(|area| {
-                let file_path = format!("{}/{}.area", *AREA_PATH, area.name);
-                area.name.clone()
-            }).collect();
-            area_stems.serialize(serializer)
-        } else {
-            serializer.serialize_unit()
+        for (stem, area) in areas {
+            let area_path = format!("{}/{}.area", *AREA_PATH, stem);
+            let area_content = serde_json::to_string_pretty(area).unwrap();
+            fs::write(area_path, area_content).unwrap();
         }
+        areas.keys()
+            .collect::<Vec<&String>>()
+            .serialize(serializer)
     }
 
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Vec<Area>>, D::Error>
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<HashMap<String, Area>, D::Error>
     where D: Deserializer<'de>,
     {
         let stems = Vec::<String>::deserialize(deserializer)?;
-        let mut loaded = vec![];
+        let mut loaded = HashMap::new();
 
         for stem in stems {
             let file_path = format!("{}/{}.area", *AREA_PATH, stem);
             let content = fs::read_to_string(file_path).map_err(serde::de::Error::custom)?;
             let content: Area = serde_json::from_str(&content).map_err(serde::de::Error::custom)?;
-            loaded.push(content);
+            loaded.insert(stem, content);
         }
 
-        Ok(Some(loaded))
+        Ok(loaded)
     }
 }
 
@@ -51,12 +51,18 @@ pub(crate) struct Area {
     pub(crate) name: String,
     description: String,
     #[serde(with = "area_room_serialization")]
-    rooms: Option<Vec<Room>>,
+    rooms: HashMap<String, Room>,
 }
 
 impl Tickable for Area {
     fn tick(&mut self, uptime: u64) {
         
+    }
+}
+
+impl Area {
+    pub(crate) fn find_room(&self, name: &str) -> Option<&Room> {
+        self.rooms.get(name)
     }
 }
 
@@ -71,15 +77,11 @@ mod area_tests {
     #[test]
     fn load_area() {
         let _ = env_logger::try_init();
-        let contents = std::fs::read_to_string(PathBuf::from(format!("{}/areas/root.area", std::env::var("RUSTROM_DATA").unwrap()))).expect("Cannot find?!");
+        let contents = std::fs::read_to_string(PathBuf::from(format!("{}/root.area", *AREA_PATH))).expect("Cannot find?!");
         debug!("Con10z:\n{}", contents);
         let area: Area = serde_json::from_str(&contents).unwrap();
         debug!("Area name: \"{}\"", area.name);
         debug!("     desc: \"{}\"", area.description);
-        if let Some(rooms) = &area.rooms {
-            for room in rooms.into_iter() {
-                debug!("{}", room.description());
-            }
-        }
+        
     }
 }
--- src/world/room.rs
+++ src/world/room.rs
@@ -1,41 +1,49 @@
+//! "Make room!" - the [Room] live here.
+use std::sync::Arc;
+
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
+use crate::DATA_PATH;
+
+static ROOM_PATH: Lazy<Arc<String>> = Lazy::new(|| Arc::new(format!("{}/rooms", *DATA_PATH)));
+
+/// Room serializer for [Area]-level hashmap.
 pub(crate) mod area_room_serialization {
-    use std::fs;
+    use std::{collections::HashMap, fs};
 
     use serde::{Deserialize, Deserializer, Serialize, Serializer};
-    use crate::world::area::AREA_PATH;
 
-    use super::Room;
+    use super::{Room, ROOM_PATH};
 
-    pub fn serialize<S: Serializer>(rooms: &Option<Vec<Room>>, serializer: S) -> Result<S::Ok, S::Error>
+    pub fn serialize<S: Serializer>(rooms: &HashMap<String, Room>, serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer,
     {
-        if let Some(rooms) = rooms {
-            let room_stems: Vec<String> = rooms.iter().map(|room| {
-                let file_path = format!("{}/{}.room", *AREA_PATH, room.name);
-                room.name.clone()
-            }).collect();
-            room_stems.serialize(serializer)
-        } else {
-            serializer.serialize_unit()
+        for (stem, room) in rooms {
+            let path = format!("{}/{}.room", *ROOM_PATH, stem);
+            let contents = serde_json::to_string_pretty(room).unwrap();
+            fs::write(path, contents).unwrap();
         }
+
+        rooms.keys()
+            .collect::<Vec<&String>>()
+            .serialize(serializer)
     }
 
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Vec<Room>>, D::Error>
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<HashMap<String, Room>, D::Error>
     where D: Deserializer<'de>,
     {
         let room_stems = Vec::<String>::deserialize(deserializer)?;
-        let mut loaded_rooms = vec![];
+        let mut loaded_rooms: HashMap<String, Room> = HashMap::new();
 
         for stem in room_stems {
-            let file_path = format!("{}/{}.room", *AREA_PATH, stem);
-            let content = fs::read_to_string(file_path).map_err(serde::de::Error::custom)?;
-            let room: Room = serde_json::from_str(&content).map_err(serde::de::Error::custom)?;
-            loaded_rooms.push(room);
+            let path = format!("{}/{}.room", *ROOM_PATH, stem);
+            let contents = fs::read_to_string(path).map_err(serde::de::Error::custom)?;
+            let room: Room = serde_json::from_str(&contents).map_err(serde::de::Error::custom)?;
+            loaded_rooms.insert(stem.to_string(), room);
         }
 
-        Ok(Some(loaded_rooms))
+        Ok(loaded_rooms)
     }
 }
 
