c6c7fa04a99e61b3ec166ee83b64609bbcd9f293
Markku Sukanen <markku.sukanen@gmail.com>
2025-08-13 23:56:54 +0300

    streamlined ClientState

--- src/util/clientstate.rs
+++ src/util/clientstate.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, Clone)]
 pub enum EditorMode {
-    Room { id: String },
-    Help { topic: String },
+    Room,
+    Help,
 }
 
 #[derive(Debug, Clone)]
@@ -12,22 +12,4 @@ pub enum ClientState {
     Playing,
     Editing { mode: EditorMode },
     Logout,
-}
-
-impl EditorMode {
-    pub(crate) fn get(&self, func_name: &str) -> Option<String> {
-        match self {
-            Self::Help { .. } => match func_name {
-                "save" => Some("hedit-save".into()),
-                "exit" => Some("hedit-exit".into()),
-                _ => None
-            },
-
-            Self::Room { .. } => match func_name {
-                "save" => Some("redit-save".into()),
-                "exit" => Some("redit-exit".into()),
-                _ => None
-            }
-        }
-    }
 }
