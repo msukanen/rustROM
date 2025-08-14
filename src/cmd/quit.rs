7881404f36a10a703b07e92793664f2adae48a38
Markku Sukanen <markku.sukanen@gmail.com>
2025-08-14 14:56:16 +0300

    marked ctx unused for 'quit'.

--- src/cmd/quit.rs
+++ src/cmd/quit.rs
@@ -5,7 +5,7 @@ pub struct QuitCommand;
 
 #[async_trait]
 impl Command for QuitCommand {
-    async fn exec(&self, ctx: &mut CommandCtx<'_>) -> ClientState {
+    async fn exec(&self, _: &mut CommandCtx<'_>) -> ClientState {
         ClientState::Logout
     }
 }
