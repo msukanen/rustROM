3a129c5d20ca7d5245bce29b265e91195a40937c
Markku Sukanen <markku.sukanen@gmail.com>
2025-08-15 14:25:53 +0300

    impl PartialEq between ClientStates

--- src/util/clientstate.rs
+++ src/util/clientstate.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum EditorMode {
     Room,
     Help,
@@ -12,4 +12,23 @@ pub enum ClientState {
     Playing,
     Editing { mode: EditorMode },
     Logout,
+}
+
+impl PartialEq for ClientState {
+    fn eq(&self, other: &Self) -> bool {
+        match self {
+            Self::EnteringName => match other { Self::EnteringName => true,_=> false },
+            Self::EnteringPassword1 { .. } => match other { Self::EnteringPassword1 { .. } => true,_=> false },
+            Self::EnteringPasswordV { .. } => match other { Self::EnteringPasswordV { .. } => true,_=> false },
+            Self::Playing => match other { Self::Playing => true,_=> false },
+            Self::Logout => match other { Self::Logout => true,_=> false },
+            Self::Editing { mode } => {
+                let mode1 = mode;
+                match other {
+                    Self::Editing { mode } => *mode1 == *mode,
+                    _ => false
+                }
+            }
+        }
+    }
 }
