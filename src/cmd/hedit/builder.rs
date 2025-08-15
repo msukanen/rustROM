431f9e100ff943cff73bd7595371c5cdf2387d23
Markku Sukanen <markku.sukanen@gmail.com>
2025-08-15 23:30:52 +0300

    - auto-save 'builder' flag changes. - info builder about save error, if happens.

--- src/cmd/hedit/builder.rs
+++ src/cmd/hedit/builder.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use crate::{cmd::{help::HelpCommand, Command, CommandCtx}, rerun_with_help, resume_game, string::boolean::BooleanCheckExt, tell_user, validate_builder, ClientState};
+use crate::{cmd::{help::{HelpCommand, ERROR_SAVING_HELP}, Command, CommandCtx}, rerun_with_help, resume_game, string::boolean::BooleanCheckExt, tell_user, traits::save::DoesSave, validate_builder, ClientState};
 
 pub struct BuilderCommand;
 
@@ -27,9 +27,12 @@ impl Command for BuilderCommand {
         let mut g = ctx.player.write().await;
         let g = g.hedit.as_mut().unwrap();
         g.dirty = true;
-        let mut g = g.lock.write().await;
-        g.builder = ctx.args.is_true();
-        tell_user!(ctx.writer, "Builder flag is now {}.\n", if g.builder {"set"} else {"unset"});
+        let mut h = g.lock.write().await;
+        h.builder = ctx.args.is_true();
+        tell_user!(ctx.writer, "Builder flag is now {}.\n", if h.builder {"set"} else {"unset"});
+        if let Err(_) = h.save().await {
+            tell_user!(ctx.writer, ERROR_SAVING_HELP);
+        }
 
         resume_game!(ctx);
     }
