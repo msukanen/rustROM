09c6a54d50791037576caf2ba88fe923dccb87a6
Markku Sukanen <markku.sukanen@gmail.com>
2026-03-08 07:30:54 +0200

    ( ... )

--- src/string/unicode.rs
+++ src/string/unicode.rs
@@ -1,2 +1,2 @@
-pub const CHECKMARK: &str = "<c green>\u{2713}</c>";
-pub const FAILMARK: &str = "<c red>\u{2717}</c>";
+pub const CHECKMARK: &str = "<c green>\u{2713}</c>"; // 
+pub const FAILMARK: &str = "<c red>\u{2717}</c>";    // 
