3743ca456c5e91bebec5d8c58cbecc75c0ab6552
Markku Sukanen <markku.sukanen@gmail.com>
2026-03-10 18:38:05 +0200

    fleshing out "inventory" command.

--- src/cmd/inventory.rs
+++ src/cmd/inventory.rs
@@ -1,17 +1,32 @@
 use async_trait::async_trait;
-use crate::{cmd::{Command, CommandCtx}};
+use crate::{cmd::{Command, CommandCtx}, item::inventory::{Storage, StorageCapacity}, tell_user};
 
 pub struct InventoryCommand;
 
 #[async_trait]
 impl Command for InventoryCommand {
     async fn exec(&self, ctx: &mut CommandCtx<'_>) {
-        let contents = {
-            let p = ctx.player.read().await;
-            p.inventory.clone()
-        };
+        let p = ctx.player.read().await;
+        let inv = &p.inventory;
 
-        todo!("Todo inventory!");
-        return;
+        if inv.is_empty() {
+            tell_user!(ctx.writer, "Your inventory seems to be <c brown>empty</c>…\n");
+            return ;
+        }
+
+        let mut output = format!(
+            "<c yellow>Inventory</c> (Slots: {}/{} | Total Objects: {})\n",
+            inv.items().len(), inv.capacity(), inv.num_items()
+        );
+        
+        for (id, item) in inv.items() {
+            let extra = if item.num_items() > 0 {
+                format!(" [contains {} items]", item.num_items())
+            } else {"".into()};
+
+            output.push_str(&format!("  - <c cyan>{id}</c>{extra}\n",));
+        }
+
+        tell_user!(ctx.writer, "{}", output);
     }
 }
