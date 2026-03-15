5df9de7495bf61ddc3b58c04a0180b5fbac12cc7
Markku Sukanen <markku.sukanen@gmail.com>
2026-03-15 23:05:16 +0200

    pipelined Room saving regardless if Room is brand new or an existing one.

--- src/cmd/redit/save.rs
+++ src/cmd/redit/save.rs
@@ -2,7 +2,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use tokio::sync::RwLock;
+use tokio::sync::{RwLock, RwLockWriteGuard};
 
 use crate::{cmd::{Command, CommandCtx}, tell_user, traits::{Identity, save::DoesSave}, validate_builder, world::room::Room};
 
@@ -16,19 +16,24 @@ impl Command for SaveCommand {
         let mut p = ctx.player.write().await;
         if let Some(ref mut ed) = p.redit {
             let room: &mut Room = &mut ed.entry;
-            if let Err(e) = room.save().await {
-                log::error!("FATAL: save error '{e:?}'");
-                tell_user!(ctx.writer, "Something went awry…\n");
-                return;
-            }
             ed.dirty = false;
             // toss into the wild!
             let mut w = ctx.world.write().await;
-            if let Some(orig) = w.rooms.get(room.id()) {
+            let new: Arc<RwLock<Room>>;
+            let mut new_lock: RwLockWriteGuard<'_, Room>;
+            if let Err(e) = {( if let Some(orig) = w.rooms.get(room.id()) {
                 let mut lock = orig.write().await;
                 lock.shallow_copy(room);
+                lock
             } else {
-                w.rooms.insert(room.id().into(), Arc::new(RwLock::new(room.clone())));
+                new = Arc::new(RwLock::new(room.clone()));
+                w.rooms.insert(room.id().into(), new.clone());
+                new_lock = new.write().await;
+                new_lock
+            }).save().await } {
+                log::error!("FATAL: save error '{e:?}'");
+                tell_user!(ctx.writer, "Something went awry…\n");
+                return;
             }
 
             tell_user!(ctx.writer, "Edits stored.\n");
