033d2e70b81dfb33ddcb44499fdce12460b13f81
Markku Sukanen <markku.sukanen@gmail.com>
2026-03-25 19:33:30 +0200

    make ExitState to handle open/close/lock/unlock directly.

--- src/world/exit.rs
+++ src/world/exit.rs
@@ -4,7 +4,14 @@ use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+/// All sorts of key related errors…
+#[derive(Debug, Clone, Copy)]
+pub enum KeyError {
+    /// Key wasn't correct, obviously.
+    IncorrectKey,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub enum ExitState {
     AlwaysOpen,
     Open { key_id: Option<String> },
@@ -20,6 +27,95 @@ impl Display for ExitState {
             Self::Open{..}   => "open",
             Self::Locked{..} => "locked tight"
         })
+    }
+}
+
+impl ExitState {
+    /// Open the exit, if possible.
+    /// 
+    /// # Returns
+    /// `true` if state changed.
+    pub fn open(&mut self) -> bool {
+        match self {
+            Self::AlwaysOpen    |
+            Self::Open {..}     |
+            Self::Locked { .. } => false,
+            Self::Closed { key_id } => { *self = Self::Open { key_id: key_id.clone() }; true },
+        }
+    }
+
+    /// Close the exit, if possible.
+    /// 
+    /// # Returns
+    /// `true` if state changed.
+    pub fn close(&mut self) -> bool {
+        match self {
+            Self::Open { key_id } => { *self = Self::Closed { key_id: key_id.clone() }; true },
+            _ => false
+        }
+    }
+
+    /// Lock the exit if possible.
+    /// 
+    /// # Returns
+    /// `true` if state changed.
+    fn autolock(&mut self) -> bool {
+        match self {
+            Self::Open { key_id: Some(key_id) }   |
+            Self::Closed { key_id: Some(key_id) } => { *self = Self::Locked { key_id: key_id.clone() }; true },
+            _ => false
+        }
+    }
+
+    /// Lock the exit with `key_id`.
+    /// 
+    /// # Returns
+    /// `true` if state changed.
+    pub fn lock_with(&mut self, key_id: &str) -> Result<bool, KeyError> {
+        match self {
+            Self::Closed { key_id: Some(lock_key_id) } |
+            Self::Open { key_id: Some(lock_key_id) }   =>
+                if lock_key_id == key_id {
+                    Ok(self.autolock())
+                } else {
+                    Err(KeyError::IncorrectKey)
+                },
+            _ => Ok(false)
+        }
+    }
+
+    /// Unlock the exit with specified key, if possible, and leave it (when applicable) at [closed][ExitState::Closed] state.
+    /// 
+    /// # Returns
+    /// `true` if state changed.
+    pub fn unlock_with(&mut self, with_key_id: &str) -> Result<bool, KeyError> {
+        match self {
+            Self::Locked { key_id } =>
+                if key_id == with_key_id {
+                    *self = Self::Closed { key_id: Some(key_id.clone()) };
+                    Ok(true)
+                } else {
+                    Err(KeyError::IncorrectKey)
+                },
+            _ => Ok(false)
+        }
+    }
+
+    /// Check whether the exit can be closed.
+    pub fn can_close(&self) -> bool {
+        !matches!(self, Self::AlwaysOpen)
+    }
+
+    /// Check whether the exit can be locked.
+    pub fn can_lock(&self) -> bool {
+        if !self.can_close() { return false; }
+
+        match self {
+            Self::Open { key_id: Some(_) }  |
+            Self::Closed { key_id: Some(_) }|
+            Self::Locked {..} => true,
+            _ => false
+        }
     }
 }
 
