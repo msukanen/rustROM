5ed82ae0ad4db3dbc1648ee009a96e6bc75f4226
Markku Sukanen <markku.sukanen@gmail.com>
2026-03-26 21:22:09 +0200

    Prep for exit "jamming" functionality.

---
+++ src/world/exit/jam.rs
@@ -0,0 +1,24 @@
+//! Jamming of doors, latches, locks, etc.
+
+use serde::{Deserialize, Serialize};
+/// Methods of jamming.
+// No jam here though, berry or otherwise.
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
+pub enum JammingMethod {
+
+}
+
+/// State of jam.
+// Edible isn't one of the options…
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
+pub enum JamState {
+    Ok,
+    LockOnly(JammingMethod),
+    WholeExit(JammingMethod),
+}
+
+impl Default for JamState {
+    fn default() -> Self {
+        Self::Ok
+    }
+}
